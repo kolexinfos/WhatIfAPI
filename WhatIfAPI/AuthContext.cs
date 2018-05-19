0a3097859bbb17adf9a0072d85edd50ac397d8b9
Kolade Kehinde <kokehind@microsoft.com>
2018-05-19 22:02:50 +0100

    first commit

---
+++ WhatIfAPI/AuthContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using VanHackAPI.Models;
+
+namespace VanHackAPI
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
